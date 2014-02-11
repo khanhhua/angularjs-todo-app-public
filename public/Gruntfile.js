53cc653bad517bc089dec993f290acf6377c711c
khanh <khanh@pingu-upc.(none)>
2014-02-11 18:14:21 +0700

    UI's

---
+++ public/Gruntfile.js
@@ -0,0 +1,55 @@
+'use strict';
+var path = require('path');
+var LIVERELOAD_PORT = 8001;
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    connect: {
+      options: {
+          port: 8000,
+          base: '.'
+      },
+      livereload: {
+        options: {
+          livereload: LIVERELOAD_PORT
+        },
+        middleware: function(connect, options) {
+            var middlewares = [];
+            if (!Array.isArray(options.base)) {
+              options.base = [options.base];
+            }
+            var directory = options.directory || options.base[options.base.length - 1];
+            options.base.forEach(function(base) {
+              // Serve static files.
+              middlewares.push(connect.static(base));
+            });
+            // Make directory browse-able.
+            middlewares.push(connect.directory(directory));
+            middlewares.push(require('connect-livereload')({ port: LIVERELOAD_PORT }));
+            return middlewares;
+        }
+      }
+    },
+    watch: {
+      livereload: {
+        options: {
+          livereload: LIVERELOAD_PORT
+        },
+        files: [
+          './{,*/}*.html',
+          './css/*.css',
+          './js/*.js'
+        ]
+      }
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-livereload');
+
+  grunt.registerTask('default',function (target) {
+    return grunt.task.run([
+      'connect:livereload',
+      'watch']) // THIS SHALL KEEP THE SERVER OPEN
+  });
+}

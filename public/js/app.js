2d7ea79833924c13678f6a38675f3f2b333c293c
Khanh Hua <giakhanh2487@gmail.com>
2014-02-13 22:09:43 +0700

    step-1.3 AngularJS most basic setup: Required scripts, app-module and controller definition, HTML view file

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,16 @@
-
+/**
+* todo Module
+*
+* The main module for this TODO app
+*/
+angular.module('todoApp', []).
+    controller('todoMainCtrl',['$scope', function($scope) {
+      // todoList array
+      $scope.todoList = [
+        {task:'Buy an SSD', due: 'Jan 1'},
+        {task:'Partition the drive', due: 'Jan 4'},
+        {task:'Install Ubuntu and Windows', due: 'Jan 9'},
+        {task:'Install optional packages', due: 'Jan 10'},
+        {task:'Install PyCharm', due: 'Jan 13'},
+      ];
+    }])

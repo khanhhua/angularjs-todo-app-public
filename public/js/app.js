c0da2805ef97b759b3903a8f6e893b198b27e8f0
Khanh Hua <giakhanh2487@gmail.com>
2014-03-16 22:30:39 +0700

    [full-feature] Update status of listed items using the checkbox

--- public/js/app.js
+++ public/js/app.js
@@ -7,14 +7,14 @@ angular.module('todoApp', []).
     controller('todoMainCtrl',['$scope', function($scope) {
       // todoList array
       $scope.todoList = [
-        {cat:'workplace', task:'Buy an SSD', due: '2014-01-01'},
-        {cat:'workplace', task:'Partition the drive', due: '2014-01-04'},
-        {cat:'workplace', task:'Install Ubuntu and Windows', due: '2014-01-09'},
-        {cat:'workplace', task:'Install optional packages', due: '2014-01-10'},
-        {cat:'workplace', task:'Install PyCharm', due: '2014-01-03'},
-        {cat:'homeplace', task:'Clean the floor', due: '2014-01-06'},
-        {cat:'homeplace', task:'Cook dinner', due: '2014-01-10'},
-        {cat:'homeplace', task:'Make the bed', due: '2014-01-14'}
+        {cat:'workplace', task:'Buy an SSD', due: '2014-01-01', status: 'unplanned'},
+        {cat:'workplace', task:'Partition the drive', due: '2014-01-04', status: 'unplanned'},
+        {cat:'workplace', task:'Install Ubuntu and Windows', due: '2014-02-09', status: 'unplanned'},
+        {cat:'workplace', task:'Install optional packages', due: '2014-03-10', status: 'unplanned'},
+        {cat:'workplace', task:'Install PyCharm', due: '2014-02-03', status: 'unplanned'},
+        {cat:'homeplace', task:'Clean the floor', due: '2014-03-06', status: 'unplanned'},
+        {cat:'homeplace', task:'Cook dinner', due: '2014-03-10', status: 'unplanned'},
+        {cat:'homeplace', task:'Make the bed', due: '2014-03-30', status: 'unplanned'}
       ];
 
       // Category
@@ -27,6 +27,17 @@ angular.module('todoApp', []).
       $scope.selectCategory = function(categoryId) {
         $scope.selectedCategory = categoryId;
       };
+
+      $scope.done = function(todoItem) {
+      // prev_status helps revert todoItem status
+      // TODO: update the requirement!
+        if (todoItem.status !== 'done') {
+          todoItem.prev_status = todoItem.status;
+          todoItem.status = 'done';
+        }
+        else if (todoItem.prev_status)
+          todoItem.status = todoItem.prev_status;
+      }
 
       $scope.todoList.sort(function(a,b) {
           return a.due > b.due;

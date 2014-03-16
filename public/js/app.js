ab48bf266353ce55f02a34c2dc954bf696f4ba4a
Khanh Hua <giakhanh2487@gmail.com>
2014-03-16 22:05:23 +0700

    [full-feature] List active to-do items in order of increasing due date

--- public/js/app.js
+++ public/js/app.js
@@ -7,12 +7,26 @@ angular.module('todoApp', []).
     controller('todoMainCtrl',['$scope', function($scope) {
       // todoList array
       $scope.todoList = [
-        {task:'Buy an SSD', due: 'Jan 1'},
-        {task:'Partition the drive', due: 'Jan 4'},
-        {task:'Install Ubuntu and Windows', due: 'Jan 9'},
-        {task:'Install optional packages', due: 'Jan 10'},
-        {task:'Install PyCharm', due: 'Jan 13'},
+        {cat:'workplace', task:'Buy an SSD', due: '2014-01-01'},
+        {cat:'workplace', task:'Partition the drive', due: '2014-01-04'},
+        {cat:'workplace', task:'Install Ubuntu and Windows', due: '2014-01-09'},
+        {cat:'workplace', task:'Install optional packages', due: '2014-01-10'},
+        {cat:'workplace', task:'Install PyCharm', due: '2014-01-03'},
+        {cat:'homeplace', task:'Clean the floor', due: '2014-01-06'},
+        {cat:'homeplace', task:'Cook dinner', due: '2014-01-10'},
+        {cat:'homeplace', task:'Make the bed', due: '2014-01-14'}
       ];
+
+      // Category
+      $scope.categoryList = [
+        {id:'workplace', name: 'Workplace'},
+        {id:'homeplace', name: 'Homeplace'}
+      ];
+
+      $scope.selectedCategory = 'workplace';
+      $scope.selectCategory = function(categoryId) {
+        $scope.selectedCategory = categoryId;
+      };
 
       $scope.todoList.sort(function(a,b) {
           return a.due > b.due;
